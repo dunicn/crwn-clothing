d49f908228e71e5a177bdf023ae77fc9129e58ef
dunicn <nemanjazdunic@gmail.com>
2021-02-20 12:13:04 +0100

    added className to CustomButton in collection-item component to fix the styling issue caused by adding styled components

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -20,7 +20,7 @@ import './collection-item.styles.scss';
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
-            <CustomButton onClick={() => addItem(item)} inverted>
+            <CustomButton  className="custom-button" onClick={() => addItem(item)} inverted>
                 Add to cart
             </CustomButton>
         </div>
